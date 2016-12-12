500c8ee867e55183c31a0f373c329a501167ff85
kalibin <evgenkalibin@mail.ru>
2016-12-12 09:35:47 +0500

    Final solution of module 5

---
+++ module5/js/script.js
@@ -0,0 +1,28 @@
+// console.log(document.getElementById("title"));
+// console.log(document instanceof HTMLDocument);
+
+function sayHello(event) {
+	console.log(this);
+	var name = document.getElementById("name").value;
+	var message = "<h2>Hello " + name + "!</h2>";
+
+	// document.getElementById("content").textContent = message;
+	document.getElementById("content").innerHTML = message;
+
+	if(name === "student"){
+		var title = document.querySelector("#title").textContent;
+		title += "& Lovin' it!";
+		document.querySelector("#title").textContent = title;
+	}
+}
+
+document.querySelector("button").addEventListener("click", sayHello);
+document.querySelector("body").addEventListener("mousemove",
+		function (event) {
+			if (event.shiftKey === true) {
+				console.log("x: " + event.clientX);
+				console.log("y: " + event.clientY);
+			}
+			
+		}
+	);
